8f3a12ee5bc31e6e72fc5b0bbac9e4f18cb669dd
Jesse Arstein <jesse.arstein@yahoo.com>
2018-12-07 13:25:25 -0700

    Added RC Car Code

---
+++ RC Car Code/Arduino/Arduino.ino
@@ -0,0 +1,79 @@
+/* 
+ *  Author: Jesse Arstein
+ *  This code runs the RC Car
+ *  
+ *  
+ */
+#include <Servo.h>
+//#include <String.h>
+Servo motor;
+Servo steering;
+String data;
+String motorValue;
+String steeringValue;
+int mV;
+int sV;
+int commaIndex;
+
+String inputString = "";
+int value = 0;
+int steeringMax = 165;
+int steeringMin = 95;
+void setup()                    // run once, when the sketch starts
+{
+
+  motor.attach(3);
+  delay(1);
+  steering.attach(9);
+  motor.write(80);
+  delay(2000);
+  Serial.begin(9600);            // set the baud rate to 9600, same should be of your Serial Monitor
+  pinMode(2, OUTPUT);
+  //pinMode(3, OUTPUT);
+  steering.write(125);
+}
+
+void loop()
+{
+
+    while (Serial.available() > 0)
+    {
+        char recieved = Serial.read();
+        data += recieved; 
+
+        // Process message when new line character is recieved
+        if (recieved == '\n')
+        {
+            Serial.print("Arduino Received: ");
+            Serial.print(data);
+
+            commaIndex = data.indexOf('.');
+            motorValue = data.substring(0,commaIndex);
+            steeringValue = data.substring(commaIndex + 1);
+
+            mV = motorValue.toInt();
+            sV = steeringValue.toInt();
+            Serial.println(sV);
+            Serial.println(mV);
+            if(mV <=1510 && mV >= 1350){
+               motor.write(mV);
+            }else if(mV >= 1240 && mV <=1340){
+                 motor.write(mV);
+            }
+            if(sV < steeringMax && sV >= steeringMin){
+              steering.write(sV);
+            }
+            data = ""; // Clear recieved buffer
+            motorValue = "";
+            steeringValue = "";
+            Serial.flush();
+
+
+        }
+    }
+//delay(8);
+
+
+
+  
+}
